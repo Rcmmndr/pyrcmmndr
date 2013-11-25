f5ccd61bf7dfa7b809bf8e2b95c6c984b01fa9f5
Halit Olali <halitolali@gmail.com>
2013-11-26 00:07:19 +0200

    supporting multiple types

--- pyrcmmndr/pyrcmmndr.py
+++ pyrcmmndr/pyrcmmndr.py
@@ -3,35 +3,43 @@ import json
 
 __author__ = 'codemomentum'
 
+
 class RcmmndrClient(object):
     def __init__(self, apiKey, server_url="http://api.rcmmndr.com/"):
         self.apiKey = apiKey
         self.server_url = server_url
 
-    def get_preference_url(self, user_id):
-        return self.server_url + '/api_key/' + self.apiKey + '/preference/' + str(user_id)
+    def get_root_url(self, datatype):
+        if datatype is None:
+            return self.server_url + '/api_key/' + self.apiKey
+        else:
+            return self.server_url + '/api_key/' + self.apiKey + '/type/' + datatype
 
-    def create_preference(self,user_id,item_id,preference):
-        post_content(self.get_preference_url(user_id) +'/'+str(item_id)+'/'+str(preference),None)
+    def get_preference_url(self, user_id, datatype=None):
+        return self.get_root_url(datatype) + '/preference/' + str(user_id)
 
-    def get_recommendation(self,user_id):
-        return get_content(self.server_url+'/api_key/'+self.apiKey+'/recommend/'+str(user_id))
+    def create_preference(self, user_id, item_id, preference, datatype=None):
+        post_content(self.get_preference_url(user_id,datatype=datatype) + '/' + str(item_id) + '/' + str(preference), None)
+
+    def get_recommendation(self, user_id, datatype=None):
+        return get_content(self.get_root_url(datatype) + '/recommend/' + str(user_id))
 
     def get_usage_stats(self):
-        return get_content(self.server_url+'/api_key/'+self.apiKey+'/_stats')
+        return get_content(self.get_root_url(None) + '/_stats')
 
-    def delete_preferences_of_user(self,user_id):
-        delete_content(self.get_preference_url(user_id))
+    def delete_preferences_of_user(self, user_id, datatype=None):
+        delete_content(self.get_preference_url(user_id,datatype=datatype))
 
-    def delete_all_preferences(self):
-        delete_content(self.server_url + '/api_key/' + self.apiKey + '/preference/_all')
+    def delete_all_preferences(self, datatype=None):
+        delete_content(self.get_root_url(datatype) + '/preference/_all')
 
-    def bulk_update_preferences(self,file):
+    def bulk_update_preferences(self, file, datatype=None):
         data = open(file).read()
-        post_content( self.server_url + '/api_key/' + self.apiKey + '/preference/_bulk',data,request_headers = {'Content-Type': 'text/plain'})
+        post_content(self.get_root_url(datatype) + '/preference/_bulk', data,
+                     request_headers={'Content-Type': 'text/plain'})
 
-    def get_settings(self):
-        return get_content(self.server_url+'/api_key/'+self.apiKey+'/_settings')
+    def get_settings(self, datatype=None):
+        return get_content(self.get_root_url(datatype) + '/_settings')
 
-    def update_settings(self,settings_dict):
-        post_content(self.server_url+'/api_key/'+self.apiKey+'/_settings',json.dumps(settings_dict))
+    def update_settings(self, settings_dict, datatype=None):
+        post_content(self.get_root_url(datatype) + '/_settings', json.dumps(settings_dict))
--- pyrcmmndr/test/pyrcmmndr_test.py
+++ pyrcmmndr/test/pyrcmmndr_test.py
@@ -1,70 +1,70 @@
-
 import unittest
 from pyrcmmndr import RcmmndrClient
 import json
 
 __author__ = 'codemomentum'
 
-class TestRcmmndrClient(unittest.TestCase):
 
+class TestRcmmndrClient(unittest.TestCase):
     def test_basic_flow(self):
-        pyrcmmndr=RcmmndrClient("123","http://localhost:3000")
+        pyrcmmndr = RcmmndrClient("0000000000000000000000000000000000000000001", "http://localhost:8001")
         #initialize
         pyrcmmndr.delete_all_preferences()
 
         #create some preferences
-        pyrcmmndr.create_preference(1,101,1.0)
+        pyrcmmndr.create_preference(1, 101, 1.0)
 
         #check the stats
-        stats=pyrcmmndr.get_usage_stats()
+        stats = pyrcmmndr.get_usage_stats()
         assert stats['total_preferences'] == 1
         assert stats['distinct_keys'] == 1
 
-        pyrcmmndr.create_preference(2,101,1.0)
-        pyrcmmndr.create_preference(2,102,2.0)
+        pyrcmmndr.create_preference(2, 101, 1.0)
+        pyrcmmndr.create_preference(2, 102, 2.0)
 
-        stats=pyrcmmndr.get_usage_stats()
+        stats = pyrcmmndr.get_usage_stats()
         assert stats['total_preferences'] == 3
         assert stats['distinct_keys'] == 2
 
         pyrcmmndr.delete_preferences_of_user(2)
-        stats=pyrcmmndr.get_usage_stats()
+        stats = pyrcmmndr.get_usage_stats()
         assert stats['total_preferences'] == 1
         assert stats['distinct_keys'] == 1
 
         pyrcmmndr.delete_all_preferences()
-        stats=pyrcmmndr.get_usage_stats()
+        stats = pyrcmmndr.get_usage_stats()
         assert stats['total_preferences'] == 0
         assert stats['distinct_keys'] == 0
 
-        pyrcmmndr.create_preference(1,101,5)
-        pyrcmmndr.create_preference(1,102,3)
-        pyrcmmndr.create_preference(1,103,2.5)
+        pyrcmmndr.create_preference(1, 101, 5)
+        pyrcmmndr.create_preference(1, 102, 3)
+        pyrcmmndr.create_preference(1, 103, 2.5)
 
-        pyrcmmndr.create_preference(2l,101l,2)
-        pyrcmmndr.create_preference(2l,102l,2.5)
-        pyrcmmndr.create_preference(2l,103l,5)
-        pyrcmmndr.create_preference(2l,104l,2)
+        pyrcmmndr.create_preference(2l, 101l, 2)
+        pyrcmmndr.create_preference(2l, 102l, 2.5)
+        pyrcmmndr.create_preference(2l, 103l, 5)
+        pyrcmmndr.create_preference(2l, 104l, 2)
 
-        pyrcmmndr.create_preference(3l,101l,2.5)
-        pyrcmmndr.create_preference(3l,104l,4)
-        pyrcmmndr.create_preference(3l,105l,4.5)
-        pyrcmmndr.create_preference(3l,107l,5)
+        pyrcmmndr.create_preference(3l, 101l, 2.5)
+        pyrcmmndr.create_preference(3l, 104l, 4)
+        pyrcmmndr.create_preference(3l, 105l, 4.5)
+        pyrcmmndr.create_preference(3l, 107l, 5)
 
-        pyrcmmndr.create_preference(4l,101l,5)
-        pyrcmmndr.create_preference(4l,103l,3)
-        pyrcmmndr.create_preference(4l,104l,4.5)
-        pyrcmmndr.create_preference(4l,106l,4)
+        pyrcmmndr.create_preference(4l, 101l, 5)
+        pyrcmmndr.create_preference(4l, 103l, 3)
+        pyrcmmndr.create_preference(4l, 104l, 4.5)
+        pyrcmmndr.create_preference(4l, 106l, 4)
 
-        pyrcmmndr.create_preference(5l,101l,4)
-        pyrcmmndr.create_preference(5l,102l,3)
-        pyrcmmndr.create_preference(5l,103l,2)
-        pyrcmmndr.create_preference(5l,104l,4)
-        pyrcmmndr.create_preference(5l,105l,3.5)
-        pyrcmmndr.create_preference(5l,106l,4)
+        pyrcmmndr.create_preference(5l, 101l, 4)
+        pyrcmmndr.create_preference(5l, 102l, 3)
+        pyrcmmndr.create_preference(5l, 103l, 2)
+        pyrcmmndr.create_preference(5l, 104l, 4)
+        pyrcmmndr.create_preference(5l, 105l, 3.5)
+        pyrcmmndr.create_preference(5l, 106l, 4)
 
         #get recommendations
-        recs=pyrcmmndr.get_recommendation(1)
+        recs = pyrcmmndr.get_recommendation(1)
+        print recs
 
         assert '104' in recs
 
@@ -93,21 +93,81 @@ class TestRcmmndrClient(unittest.TestCase):
 
         print pyrcmmndr.get_settings()
 
+        pyrcmmndr.create_preference(1, 101, 5, datatype="type1")
+        recs2 = pyrcmmndr.get_recommendation(1, datatype="type1")
+        print recs2
+        assert recs <> recs2
 
+        rec2 = """
+        {
+             "recommender": {
+                  "impl":"GenericUserBasedRecommender",
+                  "params":{
+                       "UserSimilarity" : {
+                            "impl":"PearsonCorrelationSimilarity"
+                       },
+                       "UserNeighborhood":{
+                            "impl":"NearestNUserNeighborhood",
+                            "params": {
+                                 "n":40
+                            }
+                       }
+                  }
+             }
+        	}
+         """
+
+        pyrcmmndr.update_settings(json.loads(rec2), datatype="type1")
+
+        print pyrcmmndr.get_settings(datatype="type1")
 
     def test_bulk_update(self):
-        pyrcmmndr=RcmmndrClient("123","http://localhost:3000")
+        pyrcmmndr = RcmmndrClient("0000000000000000000000000000000000000000001", "http://localhost:8001")
         #initialize
         pyrcmmndr.delete_all_preferences()
-        stats=pyrcmmndr.get_usage_stats()
+        stats = pyrcmmndr.get_usage_stats()
         assert stats['total_preferences'] == 0
         assert stats['distinct_keys'] == 0
 
         #bulk update
         pyrcmmndr.bulk_update_preferences('preferences.tsv')
-        stats=pyrcmmndr.get_usage_stats()
+        stats = pyrcmmndr.get_usage_stats()
         assert stats['total_preferences'] == 22
         assert stats['distinct_keys'] == 21
+
+
+    def test_create_some_load(self):
+        pyrcmmndr = RcmmndrClient("0000000000000000000000000000000000000000001", "http://localhost:8001")
+        #initialize
+        pyrcmmndr.delete_all_preferences()
+
+        #create some preferences
+        for x in range(0, 1000):
+            pyrcmmndr.create_preference(x + 1, 101, 5)
+            pyrcmmndr.create_preference(x + 1, 102, 3)
+            pyrcmmndr.create_preference(x + 1, 103, 2.5)
+
+            pyrcmmndr.create_preference(x + 2l, 101l, 2)
+            pyrcmmndr.create_preference(x + 2l, 102l, 2.5)
+            pyrcmmndr.create_preference(x + 2l, 103l, 5)
+            pyrcmmndr.create_preference(x + 2l, 104l, 2)
+
+            pyrcmmndr.create_preference(x + 3l, 101l, 2.5)
+            pyrcmmndr.create_preference(x + 3l, 104l, 4)
+            pyrcmmndr.create_preference(x + 3l, 105l, 4.5)
+            pyrcmmndr.create_preference(x + 3l, 107l, 5)
+
+            pyrcmmndr.create_preference(x + 4l, 101l, 5)
+            pyrcmmndr.create_preference(x + 4l, 103l, 3)
+            pyrcmmndr.create_preference(x + 4l, 104l, 4.5)
+            pyrcmmndr.create_preference(x + 4l, 106l, 4)
+
+            pyrcmmndr.create_preference(x + 5l, 101l, 4)
+            pyrcmmndr.create_preference(x + 5l, 102l, 3)
+            pyrcmmndr.create_preference(x + 5l, 103l, 2)
+            pyrcmmndr.create_preference(x + 5l, 104l, 4)
+            pyrcmmndr.create_preference(x + 5l, 105l, 3.5)
+            pyrcmmndr.create_preference(x + 5l, 106l, 4)
 
 
 if __name__ == '__main__':
